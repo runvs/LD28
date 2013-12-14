5f219eec4289e6a0bbcef522de41635340c0798e
Thunraz <thunraz@thunraz.com>
2013-12-14 10:16:33 +0000

    Added class item

---
+++ Code/Item.cs
@@ -0,0 +1,18 @@
+﻿
+namespace JamTemplate
+{
+    public class Item
+    {
+        public ItemType ItemType { get; private set; }
+
+        public Item(ItemType t)
+        {
+            this.ItemType = t;
+        }
+    }
+
+    public enum ItemType
+    {
+        HEAD, TORSO, HAND, FEET
+    }
+}
